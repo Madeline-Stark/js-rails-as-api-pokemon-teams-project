3d8c4fe28dfad5fc03c3b00b57264c0bf652efd2
github username <github email address>
2020-05-12 14:44:55 -0700

    Done.

---
+++ notes.md
@@ -0,0 +1,23 @@
+Building out the rest of the project
+Remember that your user stories are:
+
+When a user loads the page, they should see all trainers, with their current team of Pokemon.
+- build API to load all trainers and associated pokemon
+    - make models
+        - creating a rails api from scratch
+            - already have models, controllers etc from lab instructions
+            - add controller actions we want for API - confirm working in localhost
+    - make serializers using active model serializer so structures data nicely
+        - add to gemfile gem 'active_model_serializers'
+        - mkdir app/serializers
+        - then just reference how made serializers, controllers
+- create card function in js to fetch trainers and make card
+    on page load
+    make fetch to all trainers
+    make card for each trainer
+
+Whenever a user hits "Add Pokemon" and they have space on their team, they should get a new Pokemon.
+//can have validation in class to limit number of pokemon to 6 
+
+Whenever a user hits "Release Pokemon" on a specific Pokemon team, that specific Pokemon should be released from the team.
+You should build out just enough of your Rails API to achieve the above. You should not build out full CRUD on each model. For example, the frontend will not have the ability to create a new Trainer, so your backend should not have a POST /trainers route.
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,73 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+const main = document.querySelector("main")
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchTrainers()
+})
+
+function fetchTrainers(){
+    fetch(TRAINERS_URL)
+        .then(function(resp){
+            return resp.json() //returns JavaScript Object
+        })
+        .then(function(trainers){
+            
+            // for (const trainer of trainers.data){
+            //     makeTrainerCard(trainer)
+            // }
+            for (const trainer of trainers){
+                makeTrainerCard(trainer)
+            }
+
+           
+        })
+}
+
+
+function makeTrainerCard(trainer){
+    //MAKE CARD
+    const card = document.createElement('div')
+    card.classList += "card" //can also do card.setAttribute("class", "card")
+    card.dataset["id"] = trainer.id //https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+    //can also do card.setAttribute("data-id", trainer.id)
+    //MAKE NAME PTAG
+    const trainerName = document.createElement('p')
+    trainerName.innerText = trainer.name
+    card.appendChild(trainerName)
+  
+    //MAKE RELEASE BUTTON
+    const addPokeButton = document.createElement("button")
+    addPokeButton.dataset.trainerId = trainer.id //need camel case for hyphen
+    addPokeButton.innerText = "Add Pokemon"
+    card.appendChild(addPokeButton)
+    // TODO: ATTACH EVENT LISTENER TO BUTTON
+
+    //MAKE LIST
+    const pokemonList = document.createElement('ul')
+    //iterate through pokemon
+    //MAKE INDIVIDUAL POKEMON LIST ITEMS
+    for (const pokemon of trainer.pokemons) {
+        // //make li for each
+        const pokemonLi = document.createElement("li")
+        pokemonLi.innerText = pokemon.nickname
+        const releaseButton = document.createElement("button")
+        releaseButton.classList += "release"
+        releaseButton.innerText = "Release"
+        releaseButton.dataset.pokemonId = pokemon.id
+        pokemonLi.appendChild(releaseButton)
+        pokemonList.appendChild(pokemonLi)
+        //ONCE ALL DONE REFACTOR INTO OWN FUNCTION
+    } 
+    card.appendChild(pokemonList)
+    
+    //add card to DOM:
+    main.appendChild(card)
+}
+
+function renderPokemon(pokemon){
+    //would need to call this after adding list to DOM
+    //need to target specific trainer's card
+    const list = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
+}
